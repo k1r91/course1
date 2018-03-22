2b6e7e5ec53016007d238bf523b3b4e782c45388
Cherkasov Kirill <cherkasov.kirill@gmail.com>
2018-03-22 13:36:37 +0500

    TODO: make my homework.

--- l2/lesson.py
+++ l2/lesson.py
@@ -1,1 +1,18 @@
 print(1 > 0 == True)
+
+a = 25
+b = 'asdfgh'
+c = 44
+d = [33, 55, 66, 77]
+tup = (a ,b ,c, d)
+print(tup)
+b = 'ASD'
+a = 44
+d.append(1025)
+print(tup)
+
+
+for i, val in enumerate(tup, 1):
+    if type(val) == str:
+        val = ''.join([val[0].upper(), val[1:]])  # Capitalize first letter
+    print('{}. {}'.format(i, val))

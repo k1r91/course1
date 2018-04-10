2c2de56b480baa3e1f4fe5735aa26d5abb6c18c1
Kirill C <cherkasov.kirill@gmail.com>
2018-04-10 11:57:25 +0500

    Done all homework.

--- l2/homework/home_work/hw02_hard.py
+++ l2/homework/home_work/hw02_hard.py
@@ -1,8 +1,51 @@
 # Задание-1: уравнение прямой вида y = kx + b задано в виде строки.
 # Определить координату y точки с заданной координатой x.
+import math
 
-equation = 'y = -12x + 11111140.2121'
-x = 2.5
+class EquationException(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+def parse_equation(expression, x):
+    index = expression.find('x')
+    if index > 0:
+        pos = index
+        mult = ""
+        const = ""
+        while expression[pos] != ' ':
+            pos -= 1
+            if expression[pos].isdigit() or expression[pos] in '+-.':
+                mult = ''.join([mult, expression[pos]])
+            elif expression[pos] != ' ':
+                raise EquationException('Error in equation definition at x')
+        mult = mult[::-1]
+        try:
+            mult = float(mult)
+        except ValueError:
+            raise EquationException('Error in x- multiplayer defining')
+        try:
+            const = ''.join([expression[index + 2]])
+            if const not in '+-':
+                raise EquationException('Const sign must be + or -')
+        except:
+            raise EquationException('Error in const sign')
+        if expression[index + 3] != ' ':
+            raise EquationException('Missing whitespace after const sign')
+        for i in range(index + 4, len(expression)):
+            const = ''.join([const, expression[i]])
+        try:
+            const = float(const)
+        except ValueError:
+            raise EquationException("Error in const definition")
+        print('{}*{}{} = '. format(mult, x, const))
+    else:
+        raise EquationException("Can't find x")
+    return mult * x + const
 # вычислите и выведите y
 
 
@@ -24,6 +67,33 @@ date = '01.22.1001'
 date = '1.12.1001'
 date = '-2.10.3001'
 
+
+def parse_date(date):
+    print('{} is correct - '.format(date))
+    if len(date) != 10:
+        return False
+    day = date[:2]
+    month = date[3:5]
+    year = date[-4:]
+    for num in [day, month, year]:
+        if not num.isdigit():
+            return False
+    day, month, year = int(day), int(month), int(year)
+    if year < 1 or year > 9999:
+        return False
+    if month < 1 or month > 12:
+        return False
+    february_days = 28
+    if year % 400 == 0:
+        february_days = 29
+    else:
+        if year % 100 != 0 and year % 4 == 0:
+            february_days = 29
+    day_in_months = {1: 31, 2: february_days, 3: 31, 4: 30, 5: 31, 6: 30,
+                     7: 31, 8:31, 9: 30, 10: 31, 11: 30, 12: 31}
+    if day < 1 or day > day_in_months[month]:
+        return False
+    return True
 
 # Задание-3: "Перевёрнутая башня" (Задача олимпиадного уровня)
 #
@@ -54,3 +124,38 @@ date = '-2.10.3001'
 #
 # Вход: 11
 # Выход: 5 3
+
+
+def lift(room_number):
+    if room_number < 1 or room_number > 2000000000:
+        return 0, 0
+    if room_number == 1:
+        return 1, 1
+    floor = 1
+    step = 2
+    i = 1
+    while i < room_number:
+        i += step * step
+        floor += step - 1
+        step += 1
+    square = step - 1
+    room = i - square*square + 1
+    for x in range(square):
+        position = 1
+        for y in range(square):
+            if room == room_number:
+                return floor, position
+            position += 1
+            room += 1
+        floor += 1
+    return 1, 1
+
+
+if __name__ == '__main__':
+    print(parse_equation('y = 2x - 4', 2.5))
+    print(parse_date('28.02.9999'))
+    print(parse_date('01.22.1001'))
+    print(parse_date('1.12.1001'))
+    print(parse_date('-2.10.3001'))
+    print(lift(27))
+    print('Done!')
